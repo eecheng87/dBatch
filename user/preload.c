309417882d37283ee10a1ec338908628e50efdb4
eecheng <yucheng871011@gmail.com>
2021-03-10 16:11:24 +0800

    Support shutdown & writev
    WIP: import dBatch into lighttpd

--- user/preload.c
+++ user/preload.c
@@ -5,6 +5,8 @@ int main_thread_pid;
 int in_segment;
 void *mpool; /* memory pool */
 int pool_offset;
+struct iovec *iovpool; /* pool for iovector */
+int iov_offset;
 int batch_num;   /* number of busy entry */
 int syscall_num; /* number of syscall triggered currently */
 int predict_res; /* 1: do batch; 0: do normal */
@@ -187,6 +189,125 @@ ssize_t sendto(int sockfd, void *buf, size_t len, unsigned flags,
     return len;
 }
 #endif
+#if 0
+int close(int fd) {
+
+    syscall_num++;
+    if (!in_segment || !predict_res) {
+        return real_close(fd);
+    }
+    batch_num++;
+
+    int off, toff = 0, len = 0, i;
+    off = curindex[1] << 6; /* 6 = log64 */
+
+    btable[off + curindex[toff]].sysnum = __NR_close;
+    btable[off + curindex[toff]].rstatus = BENTRY_BUSY;
+    btable[off + curindex[toff]].nargs = 1;
+    btable[off + curindex[toff]].args[0] = fd;
+    btable[off + curindex[toff]].pid = main_thread_pid + off;
+    if (curindex[toff] == MAX_TABLE_SIZE - 1) {
+        if (curindex[1] == MAX_THREAD_NUM - 1) {
+            curindex[1] = 1;
+        } else {
+            curindex[1]++;
+        }
+        curindex[toff] = 1;
+    } else {
+        curindex[toff]++;
+    }
+    return 0;
+}
+#endif
+
+ssize_t shutdown(int fd, int how) {
+
+    syscall_num++;
+    if (!in_segment || !predict_res) {
+        return real_shutdown(fd, how);
+    }
+    batch_num++;
+
+    int off, toff = 0;
+    off = curindex[1] << 6; /* 6 = log64 */
+
+    btable[off + curindex[toff]].sysnum = __NR_shutdown;
+    btable[off + curindex[toff]].rstatus = BENTRY_BUSY;
+    btable[off + curindex[toff]].nargs = 2;
+    btable[off + curindex[toff]].args[0] = fd;
+    btable[off + curindex[toff]].args[1] = how;
+    btable[off + curindex[toff]].pid = main_thread_pid + off;
+
+    if (curindex[toff] == MAX_TABLE_SIZE - 1) {
+        if (curindex[1] == MAX_THREAD_NUM - 1) {
+            curindex[1] = 1;
+        } else {
+            curindex[1]++;
+        }
+        curindex[toff] = 1;
+    } else {
+        curindex[toff]++;
+    }
+
+    /* assume success */
+    return 0;
+}
+
+ssize_t writev(int fd, const struct iovec *iov, int iovcnt) {
+    syscall_num++;
+    if (!in_segment || !predict_res) {
+        return real_writev(fd, iov, iovcnt);
+    }
+    batch_num++;
+
+    int off, toff = 0, len = 0, i;
+    off = curindex[1] << 6; /* 6 = log64 */
+
+    for(i = 0; i < iovcnt; i++){
+        int ll = iov[i].iov_len;
+
+        /* handle string */
+        if (pool_offset + (ll / POOL_UNIT) > MAX_POOL_SIZE)
+            pool_offset = 0;
+        else
+            pool_offset += (ll / POOL_UNIT);
+
+        /* handle iovec */
+        if (iov_offset + 1 >= MAX_POOL_IOV_SIZE)
+            iov_offset = 0;
+        else
+            iov_offset++;
+        memcpy(mpool + pool_offset, iov[i].iov_base, ll);
+
+        iovpool[iov_offset].iov_base = mpool + pool_offset;
+        iovpool[iov_offset].iov_len = ll;
+
+        len += iov[i].iov_len;
+    }
+
+    btable[off + curindex[toff]].sysnum = __NR_writev;
+    btable[off + curindex[toff]].rstatus = BENTRY_BUSY;
+    btable[off + curindex[toff]].nargs = 3;
+    btable[off + curindex[toff]].args[0] = fd;
+    btable[off + curindex[toff]].args[1] = (long)(iovpool + iov_offset - iovcnt + 1);
+    btable[off + curindex[toff]].args[2] = iovcnt;
+    btable[off + curindex[toff]].pid = main_thread_pid + off;
+
+    if (curindex[toff] == MAX_TABLE_SIZE - 1) {
+        if (curindex[1] == MAX_THREAD_NUM - 1) {
+            curindex[1] = 1;
+        } else {
+            curindex[1]++;
+        }
+        curindex[toff] = 1;
+    } else {
+        curindex[toff]++;
+    }
+    /* assume always success */
+    //printf("-> %d\n", len);
+    return len;
+
+}
 
 ssize_t sendto(int sockfd, void *buf, size_t len, unsigned flags,
                struct sockaddr *dest_addr, int addrlen) {
@@ -253,6 +374,9 @@ __attribute__((constructor)) static void setup(void) {
     mpool = (void *)malloc(sizeof(unsigned char) * MAX_POOL_SIZE);
     pool_offset = 0;
 
+    iovpool = (struct iovec*)malloc(sizeof(struct iovec) * MAX_POOL_IOV_SIZE);
+    iov_offset = 0;
+
     /* get pid of main thread */
     main_thread_pid = syscall(186);
     btable =
@@ -264,6 +388,8 @@ __attribute__((constructor)) static void setup(void) {
     real_write = real_write ? real_write : dlsym(RTLD_NEXT, "write");
     real_read = real_read ? real_read : dlsym(RTLD_NEXT, "read");
     real_sendto = real_sendto ? real_sendto : dlsym(RTLD_NEXT, "sendto");
+    real_writev = real_writev ? real_writev : dlsym(RTLD_NEXT, "writev");
+    real_shutdown = real_shutdown ? real_shutdown : dlsym(RTLD_NEXT, "shutdown");
 
     syscall(__NR_register, btable);
 
--- user/preload.h
+++ user/preload.h
@@ -1,10 +1,11 @@
 #define _GNU_SOURCE
 
-#define DYNAMIC_PRE_ENABLE 1
+#define DYNAMIC_PRE_ENABLE 0
 
 #define MAX_TABLE_SIZE 64
 #define MAX_THREAD_NUM 10
 #define MAX_POOL_SIZE 130172
+#define MAX_POOL_IOV_SIZE 100
 #define POOL_UNIT 8
 #define BATCH_THRESHOLD 6
 
@@ -23,6 +24,8 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <sys/uio.h>
+
 
 struct pthread_fake {
     /* offset to find tid */
@@ -44,3 +47,7 @@ close_t real_close;
 typedef long (*sendto_t)(int sockfd, void *buf, size_t len, unsigned flags,
                          struct sockaddr *dest_addr, int addrlen);
 sendto_t real_sendto;
+typedef long (*writev_t)(int fd, const struct iovec *iov, int iovcnt);
+writev_t real_writev;
+typedef long (*shutdown_t)(int fd, int how);
+shutdown_t real_shutdown;
